5f9bc347a78504df639374ff3f376c2301e69e9d
sayissrao <sayissrao@hotmail.com>
2018-04-06 04:15:15 +0800

    Two new files

---
+++ sample.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number, i;
+
+    cout << "Enter a positive integer: ";
+    cin >> number;
+
+    cout << "You entered " << number;
+
+    cout << "Factors of " << number << " are: " << endl;  
+    for(i = 1; i <= number; ++i)
+    {
+        if(number % i == 0)
+            cout << i << endl;
+    }
+
+    return 0;
+}
